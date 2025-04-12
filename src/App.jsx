d5fe44e0999f99c0d40cfccb5b1824392acbeafd
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-04-12 11:53:42 +0300

    Added more profiles

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,30 @@ import { Profile } from "./components/Profile";
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
-      <Header title={"Evenning-Check-Out"} />
+      <Header title={"Profiles-Demos"} />
       <main className="p-4">
         <Profile
           name={"Kasongo"}
           title={"Thief"}
-          location={"Nairobi"}
+          location={"Nairobi-Kenya"}
           avatar={
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNarWcg5j8UmFHsLxd_k4CUI-SfM9YbxBkxg&s"
+          }
+        />
+        <Profile
+          name={"Route 66"}
+          title={"Mother-Road"}
+          location={"Chicago,Illinois, to Santa Monica, California"}
+          avatar={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy6PdM-tNr9qBOHzOZ6uxZWFak1AA3Gnumwg&s"
+          }
+        />
+        <Profile
+          name={"Lionel Messi"}
+          title={"GOAT"}
+          location={"Fort Lauderdale, Florida, USA"}
+          avatar={
+            "https://imageio.forbes.com/specials-images/imageserve/663e595b4509f97fdafb95f5/0x0.jpg?format=jpg&crop=383,383,x1045,y23,safe&height=416&width=416&fit=bounds"
           }
         />
       </main>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,9 +2,9 @@
 import { Avatar } from "./Avatar";
 import { Info } from "./Info";
 
-export function Profile({ name, title, location, avatar }) {
+export function Profile({ name, title, location, moreInfo, avatar }) {
   return (
-    <div className="bg-white rounded-lg shadow p-6 flex gap-4 items-center">
+    <div className="bg-white rounded-lg shadow p-6 flex gap-4 items-center mt-3.5">
       <Avatar src={avatar} alt={name} />
       <Info name={name} title={title} location={location} />
     </div>
